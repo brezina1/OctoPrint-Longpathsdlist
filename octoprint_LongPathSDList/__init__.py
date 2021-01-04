396dae7edf1d0709c9588c015c4ad0872e5d6e1c
Petttkous <32023091+Petttkous@users.noreply.github.com>
2021-01-04 09:36:07 +0100

    Update __init__.py

--- octoprint_LongPathSDList/__init__.py
+++ octoprint_LongPathSDList/__init__.py
@@ -11,8 +11,27 @@ class LongPathSDListPlugin(octoprint.plugin.OctoPrintPlugin):
         for file in comm._sdFiles:
             shortFile = file[0]
             # logging.getLogger("octoprint.plugin." + __name__).info("Sending: {f}.".format(f = "M33 " + shortFile))
+            # send M33 to get the long path, octoprint will map it to the short name automatically
             comm._enqueue_for_sending("M33 " + shortFile)
         return line
+
+    def get_update_information(self):
+            return dict(
+                longSdPaths=dict(
+                    displayName=self._plugin_name,
+                    displayVersion=self._plugin_version,
+
+                    # version check: github repository
+                    type="github_release",
+                    user="Petttkous",
+                    repo="OctoPrint-Longpathsdlist",
+                    current=self._plugin_version,
+
+                    # update method: pip
+                    pip="https://github.com/Petttkous/OctoPrint-Longpathsdlist/archive/{target_version}.zip"
+                )
+            )
+
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 def __plugin_load__():
@@ -21,5 +40,9 @@ def __plugin_load__():
 
     global __plugin_hooks__
     __plugin_hooks__ = {
-        "octoprint.comm.protocol.gcode.received": __plugin_implementation__.on_sd_list
+        "octoprint.comm.protocol.gcode.received": __plugin_implementation__.on_sd_list,
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
     }
+
+
+
